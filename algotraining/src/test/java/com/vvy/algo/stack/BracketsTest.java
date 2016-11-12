c9eb089433dd3722a30ea922f76cf334cc670953
VVY <yemelin@gmail.com>
2016-11-13 00:17:52 +0300

    parameterized brackets test

--- algotraining/src/test/java/com/vvy/algo/stack/BracketsTest.java
+++ algotraining/src/test/java/com/vvy/algo/stack/BracketsTest.java
@@ -1,28 +1,43 @@
 package com.vvy.algo.stack;
 
 import static com.vvy.algo.stack.Brackets.isBalanced;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(BlockJUnit4ClassRunner.class)
+@RunWith(Parameterized.class)
 public class BracketsTest {
+	
+	private String input;
+	private boolean expected;
 
-	@Test
-	public void testInvalid() {
-		String input = "(l)c";
-		Assert.assertTrue(isBalanced(input));
+	public BracketsTest(String input, boolean expected) {
+		this.input = input;
+		this.expected = expected;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> params() {
+		return Arrays.asList(new Object[][] {
+				{"",true},
+				{"abc", true},
+				{"{ab[()b]}",true},
+				{"(l)c",true},
+				{"{[]]",false},
+				{"{[()",false},
+				{"{][}", false},
+				{"{[()}]",false},
+		});
 	}
 	
 	@Test
 	public void testSequences() {
-		Assert.assertTrue(isBalanced(""));
-		Assert.assertTrue(isBalanced("abc"));
-		Assert.assertTrue(isBalanced("{ab[()b]}"));
-		Assert.assertFalse(isBalanced("{[]]"));
-		Assert.assertFalse(isBalanced("{[()"));
-		Assert.assertFalse(isBalanced("{][}"));
-		Assert.assertFalse(isBalanced("{[()}]"));
+		Assert.assertEquals(expected, isBalanced(input));
 	}
 }
